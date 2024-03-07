d928b0711fdcc31b3ef07db4913638bb1ec1a61c
SUman <sumanm2003@gmail.com>
2024-03-07 16:13:36 +0530

    first commit

--- model_data/main.py
+++ model_data/main.py
@@ -2,8 +2,7 @@ from Detector import *
 import os
 
 def main():
-    videoPath = 0
-
+    videoPath = r"C:\Users\laksu\OneDrive\Desktop\HS\paper publication\projects\object_detection\Object-Detection\test_video\video.mp4"
     configPath = os.path.join("model_data","ssd_mobilenet_v3_large_coco_2020_01_14.pbtxt")
     modelPath = os.path.join("model_data","frozen_inference_graph.pb")
     classesPath = os.path.join("model_data","coco.names")
